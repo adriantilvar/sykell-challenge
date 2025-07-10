78fc3c3dd7b77d537299f39f9d7db069e0d4a024
adriantilvar <62797798+adriantilvar@users.noreply.github.com>
2025-07-10 08:29:06 +0200

    Initial setup of Go API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
-export default function Home() {
+type UrlInfo = {
+  id: string;
+  htmlVersion: string;
+  pageTitle: string;
+  h1Count: number;
+  h2Count: number;
+  h3Count: number;
+  h4Count: number;
+  internalLinksCount: number;
+  externalLinksCount: number;
+  brokenLinksCount: number;
+  hasLoginForm: boolean;
+};
+
+export default async function Home() {
+  // TODO: Validate response
+  const urls = await fetch("http://127.0.0.1:1534/urls", {
+    cache: "no-store",
+  }).then((res) => res.json());
+
   return (
-      <main className="h-full flex justify-center items-center">
-        <h1 className="text-xl bg-orange-200 px-2 py-1">Page under construction</h1>
-      </main>
+    <main className="flex h-full flex-col items-center justify-center">
+      <h1 className="bg-orange-200 px-2 py-1 text-xl">
+        Page under construction
+      </h1>
+
+      <ul>
+        {urls.map((url: UrlInfo) => (
+          <li key={url.id}>{url.pageTitle}</li>
+        ))}
+      </ul>
+    </main>
   );
 }
